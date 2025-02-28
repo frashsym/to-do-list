f70d92f23f5edd075f660cb8e31afafca6cea313
frashsym <rfqfrashsym@gmail.com>
2025-02-28 11:26:57 +0700

    pw harus 8 char

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -43,7 +43,7 @@ class ProfileController extends Controller
     public function destroy(Request $request): RedirectResponse
     {
         $request->validateWithBag('userDeletion', [
-            'password' => ['required', 'current_password'],
+            'password' => ['required', 'current_password', 'min:8'],
         ]);
 
         $user = $request->user();
